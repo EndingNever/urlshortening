5424dc1770205edb13c5cafda7cd0100ee08dd78
Fabian Villasenor <exionite@gmail.com>
2022-06-02 11:23:36 -0700

    hero image

--- src/Components/MoreThanJust/MoreThanJust.js
+++ src/Components/MoreThanJust/MoreThanJust.js
@@ -1,12 +1,16 @@
 import React from 'react'
-import { MoreThanJustContainer } from '../../Styles'
+import { MoreThanJustContainer, CyanButton, CyanStarted } from '../../Styles'
 import "./MoreThanJust.scss"
 import working from "../../images/working.svg"
 
 export default function MoreThanJust() {
   return (
     <MoreThanJustContainer>
-      <div className="left-container">Left</div>
+      <div className="left-container">
+        <h1>More than just shorter links</h1>
+        <h2>Build your brand's recognition and get detailed insights on how your links are performing.</h2>
+        <CyanStarted>Get Started</CyanStarted>
+      </div>
       <div className="right-container">
         <img src={working} alt="" />
       </div>
--- src/Styles.js
+++ src/Styles.js
@@ -13,16 +13,36 @@ export const CyanButton = styled.button`
       margin-bottom: auto;
 `
 
+export const CyanStarted = styled(CyanButton)`
+margin-bottom: 0px;
+margin-left: 0px;
+text-align: center;
+font-size: 18px;
+height: 60px;
+width: 175px;
+`
+
 export const MoreThanJustContainer = styled.div`
 margin-top: 32px;
 display: flex;
 justify-content: flex-end;
-border: 2px solid red;
+font-family: "Poppins";
 .left-container{
-      border: 2px solid red;
+      width: 50%;
+      h1{
+            font-size: 80px;
+            line-height: 1.2;
+            margin-bottom: 0px;
+      }
+      h2{
+            color: var(--gray);
+            margin-top: 0px;
+      }
 }
 .right-container{
-      border: 2px solid red;
+      display: flex;
+      justify-content: flex-end;
+      width: 50%;
 }
 
 `
